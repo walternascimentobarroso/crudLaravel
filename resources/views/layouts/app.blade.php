8e555e8860b86c2435034bda6741d8bca9835193
Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>
2021-12-16 10:23:12 -0400

    [ADJUST] Adjust the folders
    Signed-off-by: Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,10 +22,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/">Inicio</a>
+                        <a class="nav-link {{ request()->is('/') ? 'active' : ''}}" aria-current="page" href="/">Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="/shopping-list">Lista de Compras</a>
+                        <a class="nav-link {{ request()->is('shopping-list*') ? 'active' : ''}}" href="/shopping-list">Lista de Compras</a>
                     </li>
                 </ul>
             </div>
@@ -33,22 +33,7 @@
     </nav>
     <br />
     <div class="container">
-        <form method="POST" action="/shopping-list/store">
-            @csrf
-            <div class="mb-3">
-                <label for="item" class="form-label">Item</label>
-                <input type="text" class="form-control" id="item" name="item">
-            </div>
-            <div class="mb-3">
-                <label for="price" class="form-label">Preço</label>
-                <input type="text" class="form-control" id="price" name="price">
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="checked" name="checked">
-                <label class="form-check-label" for="checked">Comprado?</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Salvar</button>
-        </form>
+        @yield('content')
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
---
+++ resources/views/shopping-list/form.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<form method="POST" action="/shopping-list/store">
+    @csrf
+    <div class="mb-3">
+        <label for="item" class="form-label">Item</label>
+        <input type="text" class="form-control" id="item" name="item">
+    </div>
+    <div class="mb-3">
+        <label for="price" class="form-label">Preço</label>
+        <input type="text" class="form-control" id="price" name="price">
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="checked" name="checked">
+        <label class="form-check-label" for="checked">Comprado?</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Salvar</button>
+</form>
+@endsection
