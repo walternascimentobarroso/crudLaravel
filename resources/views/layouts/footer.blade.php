7cd318ce11edf409de843e4ee187737e5576d7d2
Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>
2021-12-18 15:09:53 -0400

    [ADD] Menu, footer and breadcrumb
    Signed-off-by: Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="d-flex justify-content-between align-itens-center py-3 my-4 border-top">
+    <p class="mb-0 text-muted">© {{ date('Y') }} - Curso de Laravel</p>
+    <!-- © {{ now()->year }} - Curso de Laravel -->
+    <!-- <?php echo date('Y') ?> -->
+
+    <ul class="nav">
+        <li class="nav-item"> <a href="/" class="nav-link text-muted"> Inicio</a></li>
+        <li class="nav-item"> <a href="shopping-list" class="nav-link text-muted"> Lista de compras</a></li>
+    </ul>
+</footer>
